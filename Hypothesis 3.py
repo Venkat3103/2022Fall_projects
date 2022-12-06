e88b40984dd111dc6daffc9e8843f016d128e429
Burzin <burzinwadia25@gmail.com>
2022-12-06 15:25:13 -0600

    Hypothesis 3 : Added Doctest Functions and Docstrings to all.

--- Hypothesis 3.py
+++ Hypothesis 3.py
@@ -1,19 +1,21 @@
+"""
+Hypothesis 3: Consistency in Team selection improves chances of winning the match
+
+"""
+from typing import Tuple
+
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly
-import plotly.express as px
-from matplotlib import ticker
-import numpy as np
 import plotly.graph_objects as go
+from pandas import DataFrame
+from plotly.subplots import make_subplots
 
 
-def SeasonData(df: pd.DataFrame, season: int) -> pd.DataFrame:
+def SeasonData(df: DataFrame, season: int) -> DataFrame:
     """
-    This function takes the main csv file to filter it according to the season year and gives it as output dataframe.
+    This function takes the main teamsheet.csv file to filter it according to the season year and gives it as output dataframe.
 
     df: The Team DataFrame which provides information about the players played for a team for each season
-    :season: This the year that is provided by the user. Data will be filtered according to the year mentioned.
+    :season: Year value that is provided by the user. Data will be filtered according to the year mentioned.
     return: It returns a dataframe with all data with respect to the year mentioned in the function
 
     >>> d1= SeasonData(pd.DataFrame({"Unnamed: 0": [0,1,2], "Team":["Chennai Super Kings", "Sunrisers Hyderabad", "Rajasthan Royals" ] , "date": ["2018-04-07", "2019-05-17", "2018-04-01" ], "player1": ["A1","B1","C1"] ,"player2": ["A2","B2","C2"] , "player3": ["A3","B3","C3"] , "player4": ["A4","B4","C4"] , "player5": ["A5","B5","C5"] ,"player6": ["A6","B6","C6"],"player7": ["A7","B7","C7"] ,"player8": ["A8","B8","C8"] ,"player9": ["A9","B9","C9"] ,"player10": ["A10","B10","C10"] ,"player11": ["A11","B11","C11"],"year": [2018,2019,2018]}),2018)
@@ -49,7 +51,7 @@ def SeasonData(df: pd.DataFrame, season: int) -> pd.DataFrame:
     return team_season
 
 
-def player_count(season_df : pd.DataFrame) -> pd.DataFrame:
+def PlayerCount(season_df: DataFrame) -> DataFrame:
     """
     This function takes seasonal team dataframe which consists of all the information of players for a specific
     season year. The function then aggregates and counts the number of time each player appeared for their team
@@ -58,7 +60,7 @@ def player_count(season_df : pd.DataFrame) -> pd.DataFrame:
     param season_df: The input would be the name of the dataframe for a specific season.
     :return: will return a dataframe with all the player count for each team for a specific season year.
 
-    >>> f1 = player_count(pd.DataFrame({ "Team":["Chennai Super Kings", "Sunrisers Hyderabad", "Rajasthan Royals", "Chennai Super Kings", "Chennai Super Kings", "Sunrisers Hyderabad" ] , "date": ["2018-04-07", "2019-05-17", "2018-04-01", '2018-04-03', "2018-07-01", "2018-09-12"  ], "player1": ["A1","B1","C1","A1","A1","B1"] ,"player2": ["A2","B2","C2","A2","A3","B1"] , "player3": ["A3","B3","C3","A1","A3","B2"] , "player4": ["A4","B4","C4","A4","A4","B4"] , "player5": ["A5","B5","C5","A5","A5","B4"] ,"player6": ["A6","B6","C6","A6","A7","B6"],"player7": ["A7","B7","C7","A7","A7","B7"] ,"player8": ["A8","B8","C8","A8","A8","B8"] ,"player9": ["A9","B9","C9","A9","A7","B7"] ,"player10": ["A10","B10","C10","A10","A10","B10"] ,"player11": ["A11","B11","C11","A7","A8","B7"],"year": [2018,2019,2018,2018,2018,2018]}))
+    >>> f1 = PlayerCount(pd.DataFrame({ "Team":["Chennai Super Kings", "Sunrisers Hyderabad", "Rajasthan Royals", "Chennai Super Kings", "Chennai Super Kings", "Sunrisers Hyderabad" ] , "date": ["2018-04-07", "2019-05-17", "2018-04-01", '2018-04-03', "2018-07-01", "2018-09-12"  ], "player1": ["A1","B1","C1","A1","A1","B1"] ,"player2": ["A2","B2","C2","A2","A3","B1"] , "player3": ["A3","B3","C3","A1","A3","B2"] , "player4": ["A4","B4","C4","A4","A4","B4"] , "player5": ["A5","B5","C5","A5","A5","B4"] ,"player6": ["A6","B6","C6","A6","A7","B6"],"player7": ["A7","B7","C7","A7","A7","B7"] ,"player8": ["A8","B8","C8","A8","A8","B8"] ,"player9": ["A9","B9","C9","A9","A7","B7"] ,"player10": ["A10","B10","C10","A10","A10","B10"] ,"player11": ["A11","B11","C11","A7","A8","B7"],"year": [2018,2019,2018,2018,2018,2018]}))
     >>> f1[f1["count"] >4]
                       Team player  count
     8  Chennai Super Kings     A7      6
@@ -121,11 +123,31 @@ def player_count(season_df : pd.DataFrame) -> pd.DataFrame:
     return count_df
 
 
-def plot_consistency(df_count):
+def PlayerConsistency(df_count: DataFrame) -> tuple[None, DataFrame]:
     """
+    This Function takes the output DataFrame from function PlayerCount() as an input and calculate the consistency aka
+    % of matches played by a player for his team throughout the season. The output is a bar chart of each team showing
+    the consistency of each player selected for all the matches that team played.
 
-  :param df_count:
-  """
+    param df_count: DataFrame which will provide the team name , each of its player name and how many matches the
+            player played imm total for a whole season.
+
+    >>> df = pd.DataFrame({"Team":["Chennai Super Kings", "Sunrisers Hyderabad", "Rajasthan Royals", "Chennai Super Kings", "Chennai Super Kings", "Sunrisers Hyderabad" ], "player":["A1","B1","C1","A2","A3","B2"], "count":[1,2,1,3,2,1]})
+    >>> PlayerConsistency(df)
+    (None,                   Team player  count  percentage
+    3  Chennai Super Kings     A2      3      100.00
+    4  Chennai Super Kings     A3      2       66.67
+    0  Chennai Super Kings     A1      1       33.33)
+
+
+    >>> x, y = PlayerConsistency(df)
+    >>> len(y)
+    3
+
+    >>> type(y)
+    <class 'pandas.core.frame.DataFrame'>
+
+    """
     plot_list = df_count["Team"].unique().tolist()
 
     for i in plot_list:
@@ -140,7 +162,6 @@ def plot_consistency(df_count):
         x = plot_df["player"]
         y = plot_df["percentage"]
 
-        # Use textposition='auto' for direct text
         fig = go.Figure(data=[go.Bar(
             x=x, y=y,
             text=y,
@@ -152,15 +173,214 @@ def plot_consistency(df_count):
             title_text=f'{i} : Players Selection Consistency % for each match',
             uniformtext=dict(mode="hide", minsize=10))
 
-        return fig.show()
+        return fig.show(), plot_df
+
+
+def TeamConsistency(player_count_df: pd.DataFrame, consistency_threshold: int) -> tuple[None, DataFrame]:
+    """
+    This Function takes the output DataFrame from function PlayerCount() as an input and integer from the user
+    as defined by consistency_value variable in the if_main condition. This function then calculates Team Consistency
+    i.e. % of players that players equal to or above the consistency_threshold defined by the user. The output is a
+    Team's player selection Consistency plot and its DataFrame.
+
+    param player_count_df: DataFrame which will provide the team name , each of its player name and how many matches the
+                           player played in total for a whole season.
+    param consistency_threshold: A integer describing a percentage value by the user.
+    :return: Team's player selection Consistency plot and its DataFrame
+
+    >>> df = pd.DataFrame({"Team":["Chennai Super Kings", "Sunrisers Hyderabad", "Rajasthan Royals", "Chennai Super Kings", "Chennai Super Kings", "Sunrisers Hyderabad" ], "player":["A1","B1","C1","A2","A3","B2"], "count":[1,2,1,3,2,1]})
+    >>> TeamConsistency(df, 2018)
+    []
+
+    """
+
+    cons_dict = dict()
+
+    team_list = player_count_df["Team"].unique().tolist()
+
+    for i in team_list:
+        cons_df = player_count_df[player_count_df["Team"] == i]
+
+        cons_df["percentage"] = round((cons_df["count"] / len(cons_df["count"])) * 100, 2)
+
+        cons_df.sort_values(by=['percentage'], ascending=False, inplace=True)
+
+        count = cons_df[cons_df["percentage"] >= consistency_threshold].count().player
+
+        consistency_per = round(((count / cons_df["player"].count()) * 100), 2)
+
+        cons_dict[i] = consistency_per
+
+    team_cons_df = pd.DataFrame(cons_dict.items(), columns=["Team", "Consistency of Players (%)"])
+
+    x = team_cons_df["Team"]
+    y = team_cons_df["Consistency of Players (%)"]
+
+    fig = go.Figure(data=[go.Bar(
+        x=x, y=y,
+        text=y,
+        textposition='auto',
+        marker_color='rgb(55, 83, 109)'
+    )])
+
+    fig.update_layout(
+        title_text="Team Selection Consistency % for overall season",
+        uniformtext=dict(mode="hide", minsize=10))
+
+    return fig.show(), team_cons_df
+
+# ----------------------------------------------------------------------------------------------------------------------
+def TossSeasonData(toss_df: DataFrame, season: int) -> DataFrame:
+    """
+    This function takes the ipl_match_info.csv file to filter it according to the season year and gives it as output
+    dataframe.
+
+    param df: A DataFrame with information on Teams, Fixtures, Date, Toss Decision, Toss Winner and Match Winner.
+    param season: Year value that is provided by the user. Data will be filtered according to the year mentioned.
+    :return: It returns a dataframe with all data with respect to the year mentioned in the function
+
+    ToDo: Check for datatyping of Function Parameters
+
+    >>> t_df = pd.DataFrame({"Team1":["Mumbai Indians","Kolkata Knight Riders", "Kings XI Punjab","Royal Challengers Bangalore", "Royal Challengers Bangalore", "Delhi Capitals"] , "Team2":["Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Rajasthan Royals"], "date":["2018-04-07","2018-04-10","2018-04-15", "2018-04-25", "2018-05-05", "2019-05-18"], "venue":["Wankhede Stadium","MA Chidambaram Stadium", "Punjab Cricket Association IS Bindra Stadium","M.Chinnaswamy Stadium","Maharashtra Cricket Association Stadium","Arun Jaitley Stadium"], "toss_winner":["Chennai Super Kings", "Chennai Super Kings","Chennai Super Kings","Chennai Super Kings","Chennai Super Kings","Chennai Super Kings"], "toss_decision":["field","field","field","field","field","field"], "match_winner":["Chennai Super Kings","Chennai Super Kings","Kings XI Punjab","Chennai Super Kings","Chennai Super Kings","Delhi Capitals"], "year":[2018,2018,2018,2018,2018,2019]})
+    >>> len(TossSeasonData(t_df, 2018))
+    5
+
+    >>> len(TossSeasonData(t_df, 2019))
+    1
+
+    >>> t = TossSeasonData(t_df, 2018)
+    >>> type(t["year"][0])
+    <class 'numpy.int64'>
+
+    """
+    # User input at the very beginning of the file should be the season year
+
+    # preprocessing of the dataframe
+    toss_df['date'] = pd.to_datetime(toss_df['date'])
+
+    match_season = pd.DataFrame(toss_df[toss_df["year"] == season].sort_values(by=['match_winner', 'date']))
+
+    # Returns Dataframe for a season with variable name like "season_2018" if season = 2018
+
+    return match_season
+
+def MatchesWon(toss_season_df: DataFrame) -> DataFrame:
+    """
+    The Function simply groups by Team and Counts how many matches have each team won for a season.
+
+    param toss_season_df: Takes the filtered dataframe from TossSeasonData(), data for the respective season.
+    :return: DataFrame with Team Name and Number of Matches Won by each team for the specified Season.
+
+    >>> t_df = pd.DataFrame({"Team1":["Mumbai Indians","Kolkata Knight Riders", "Kings XI Punjab","Royal Challengers Bangalore", "Royal Challengers Bangalore", "Delhi Capitals"] , "Team2":["Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Chennai Super Kings", "Rajasthan Royals"], "date":["2018-04-07","2018-04-10","2018-04-15", "2018-04-25", "2018-05-05", "2019-05-18"], "venue":["Wankhede Stadium","MA Chidambaram Stadium", "Punjab Cricket Association IS Bindra Stadium","M.Chinnaswamy Stadium","Maharashtra Cricket Association Stadium","Arun Jaitley Stadium"], "toss_winner":["Chennai Super Kings", "Chennai Super Kings","Chennai Super Kings","Chennai Super Kings","Chennai Super Kings","Chennai Super Kings"], "toss_decision":["field","field","field","field","field","field"], "match_winner":["Chennai Super Kings","Chennai Super Kings","Kings XI Punjab","Chennai Super Kings","Chennai Super Kings","Delhi Capitals"], "year":[2018,2018,2018,2018,2018,2018]})
+    >>> MatchesWon(t_df)
+              match_winner  Number of Matches Won
+    0  Chennai Super Kings                      4
+    1       Delhi Capitals                      1
+    2      Kings XI Punjab                      1
+
+    >>> MatchesWon(t_df)["Number of Matches Won"].sum()
+    6
+
+    """
+    win_df = pd.DataFrame(toss_season_df.groupby(["match_winner"])["match_winner"].count())
+
+    win_df.rename(columns={"match_winner": "Number of Matches Won"}, inplace=True)
+
+    win_df.reset_index(inplace=True)
+
+    return win_df
+
+
+# -------------------------------------------------------------------------------------------------
+# Players consistent in their team VS Number of matches won by their team
+
+def hypo_plot(MatchWin_df: DataFrame, TeamConsistency_df: DataFrame):
+    """
+    Creates a Line Plot of Overall Team Selection Consistency vs Number of Matches Won by the team for a season.
+    Using this plot one can understand the explanation behind whether it statisfies the Hypothesis 3 or not.
+
+    param MatchWin_df: The output DataFrame of MatchesWon().
+    :param TeamConsistency_df: he output DataFrame of TeamConsistency().
+    :return: a Line Plot of Overall Team Selection Consistency vs Number of Matches Won by the team for a season.
+
+    """
+    plot_df = MatchWin_df.merge(TeamConsistency_df, left_on="match_winner", right_on="Team", how="inner")
+    plot_df.sort_values(by="Consistency of Players (%)", inplace=True)
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Add traces
+    fig.add_trace(
+        go.Scatter(x=plot_df["Team"], y=plot_df["Consistency of Players (%)"],
+                   name="Overall Team Consistency"),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=plot_df["match_winner"], y=plot_df["Number of Matches Won"], name="Matches Won"),
+        secondary_y=True,
+    )
+
+    # Add figure title
+    fig.update_layout(
+        title_text=f"Player's selection consistency VS Number of matches won by their team : Season {season_to_check}"
+    )
+
+    # Set x-axis title
+    #fig.update_xaxes(title_text="Team")
+    fig.update_xaxes(
+        tickangle=45,
+        title_text="Team",
+        title_font={"size": 20},
+        title_standoff=25)
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text="Team Consistency (%)", secondary_y=False)
+    fig.update_yaxes(title_text="Number of Matches Won", secondary_y=True)
+    # fig.update_layout(xaxis={'categoryorder': 'total descending'})
+
+    # return plot_df["Number of Matches Won"]
+    return fig.show()
 
 
 if __name__ == "__main__":
+    # Defining Parameters
+
+    # Which IPL season do you want to check the statistics for ?
+    season_to_check = 2018
+
+    # Define the % of matches a player played for his team for a season to count him towards team consistency?
+    consistency_value = 80
+
+    # ---------------------------------------------------------------------------------------------------------------------
+    # Dataframe 1 : Information of matches, teams and players who played for their team  each of the match.
     team_df = pd.read_csv("teamsheet.csv")
 
-    season_df = SeasonData(team_df,2018)
+    # Variable to store the dataframe after applying the year filter (season to check )
+    team_season_data = SeasonData(team_df, season_to_check)
 
-    count_df = player_count(season_df)
+    # Variable to store the number of times a player played for his team for the season specified
+    player_count = PlayerCount(team_season_data)
 
-    plot_consistency(count_df)
+    # Bar Plot of the % of matches a player played for his team for the season specified
+    Pconsistency_plot, Pconsistency_df = PlayerConsistency(player_count)
+    print(Pconsistency_df)
 
+    # A Bar Plot of % of Team Consistency, with individual player consistency >= consistency_value of all the players
+    # for each team
+    consistency_plot, team_consistency = TeamConsistency(player_count, consistency_value)
+    # --------------------------------------------------------------------------------------------------------------------
+
+    # Dataframe 2 : Information of matches, toss decision and match winner for specified season
+    toss_df = pd.read_csv("ipl_match_info.csv")
+
+    # Variable to filter and store toss_df data for a specific season.
+    toss_season_data = TossSeasonData(toss_df, season_to_check)
+
+    # This variable will store a dataframe with team name and its number of wins for that season as two columns.
+    win_info = MatchesWon(toss_season_data)
+
+    # -------------------------------------------------------------------------------------------------------------------
+
+    # Line plot of relation between Team consistency vs Number of Matches won by that team throughout the season.
+    hypo_plot(win_info, team_consistency)
--- Hypothesis 4.py
+++ Hypothesis 4.py
@@ -6,66 +6,152 @@ import plotly.express as px
 from matplotlib import ticker
 import numpy as np
 
-toss_df = pd.read_csv("ipl_match_info.csv", usecols=['Team1', 'Team2', 'date', 'venue', 'toss_winner', 'toss_decision'
-    , 'match_winner', 'year'])
 
-toss_df['Team1'] = toss_df['Team1'].replace(['Delhi Daredevils'], 'Delhi Capitals')
-toss_df['Team2'] = toss_df['Team2'].replace(['Delhi Daredevils'], 'Delhi Capitals')
-toss_df['toss_winner'] = toss_df['toss_winner'].replace(['Delhi Daredevils'], 'Delhi Capitals')
-toss_df['match_winner'] = toss_df['match_winner'].replace(['Delhi Daredevils'], 'Delhi Capitals')
+def process(unprocessed_df, season):
+    """
 
-# Number of Toss won by each Team during the IPL Season of 2018, 2019, 2020
+    :param unprocessed_df:
+    :return:
+    """
+    if season != "all":
+        unprocessed_df = unprocessed_df[unprocessed_df["year"] == season]
 
-toss_team = pd.DataFrame(toss_df.groupby(["toss_winner", "toss_decision"])["toss_winner"].count())
-toss_team = toss_team.rename(columns={'toss_winner': 'Number of Tosses Won'})
-toss_team.reset_index(inplace=True)
-toss_team.head()
+    else:
+        # Preprocess Data for all the year
+        unprocessed_df['Team1'] = unprocessed_df['Team1'].replace(['Delhi Daredevils'], 'Delhi Capitals')
+        unprocessed_df['Team2'] = unprocessed_df['Team2'].replace(['Delhi Daredevils'], 'Delhi Capitals')
+        unprocessed_df['toss_winner'] = unprocessed_df['toss_winner'].replace(['Delhi Daredevils'], 'Delhi Capitals')
+        unprocessed_df['match_winner'] = unprocessed_df['match_winner'].replace(['Delhi Daredevils'], 'Delhi Capitals')
 
-fig = px.bar(toss_team, x='toss_winner', y='Number of Tosses Won', color='toss_decision',
-             text=toss_team['Number of Tosses Won'].astype(str),
-             title="Number of Tosses won for each type and each team")
+    return unprocessed_df
 
-fig.show()
 
-# --------------------------------------------------------------------------------------------------------------------
+def overall_plot(processed_df):
+    """
 
-# How does Toss Decision has any relation with that team winning the match or not
+    :param processed_df:
+    """
+    # Number of Toss won by each Team during the IPL Season of 2018, 2019, 2020
 
-match_won = pd.DataFrame(toss_df.groupby(["match_winner", "toss_decision"])["match_winner"].count())
-match_won = match_won.rename(columns={'match_winner': 'Number of Matches Won'})
-match_won.reset_index(inplace=True)
-match_won = match_won.rename(columns={'match_winner': "Match Winner"})
-match_won.head()
+    match_won = pd.DataFrame(processed_df.groupby(["match_winner", "toss_decision"])["match_winner"].count())
+    match_won = match_won.rename(columns={'match_winner': 'Number of Matches Won'})
+    match_won.reset_index(inplace=True)
+    match_won = match_won.rename(columns={'match_winner': "Match Winner"})
+    match_won.head()
 
-colors = plotly.colors.qualitative.Prism
-fig = px.bar(match_won, x='Match Winner', y='Number of Matches Won', color='toss_decision',
-             text=match_won['Number of Matches Won'].astype(str), color_discrete_sequence=colors)
-fig.show()
+    colors = plotly.colors.qualitative.Prism
+    fig = px.bar(match_won, x='Match Winner', y='Number of Matches Won', color='toss_decision',
+                 text=match_won['Number of Matches Won'].astype(str), color_discrete_sequence=colors)
+    fig.update_layout(title_text=f'Toss Decision vs Matches Won for :  {season} Season')
+    fig.show()
 
-# --------------------------------------------------------------------------------------------------------------------
-
-# % of games a team chose batting first, won the match
-
-winner_bat = pd.DataFrame(toss_df[["Team1", "Team2", "toss_winner", "toss_decision", "match_winner"]])
-
-winner_bat['Bat_Won'] = np.where((winner_bat['toss_winner'] == winner_bat['match_winner']) & (
-        winner_bat['toss_decision'] == "bat"), "Bat First",
-                                 np.where((winner_bat['toss_winner'] == winner_bat['match_winner']),
-                                          "Bat Second", np.nan))
-
-ax = sns.countplot(x="Bat_Won", data=winner_bat)
-
-# --------------------------------------------------------------------------------------------------------------------
 
 # % of games a team won the toss, won the match
+def toss_plot(processed_df):
+    """
 
-winner_toss = pd.DataFrame(toss_df[["toss_winner","match_winner"]])
-winner_toss
+    :param processed_df:
+    """
 
-winner_toss['Toss_Won'] = np.where((winner_toss['toss_winner'] == winner_toss['match_winner']), "WonT_WonM", np.where((winner_toss['toss_winner'] != winner_toss['match_winner']), "WonT_LostM",np.nan))
-winner_toss
+    winner_toss = pd.DataFrame(processed_df[["toss_winner", "match_winner"]])
 
-ax = sns.countplot(x="Toss_Won", data=winner_toss)
+    winner_toss['Toss_Won'] = np.where((winner_toss['toss_winner'] == winner_toss['match_winner']), "Won the Toss and "
+                                                                                                    "Won the Match",
+                                       np.where((winner_toss['toss_winner'] != winner_toss['match_winner']),
+                                                "Won the Toss and Lost the Match",
+                                                np.nan))
+    count_df = pd.DataFrame(winner_toss.groupby("Toss_Won")["Toss_Won"].count())
+    count_df.rename(columns={"Toss_Won": "count"}, inplace=True)
+    count_df.reset_index(inplace=True)
+
+    fig = px.pie(count_df, values="count", names='Toss_Won')
+
+    fig.update_layout(title_text=f'Toss results vs % of Matches Won for :  {season} Season')
+
+    fig.show()
+
+
+def decision_plot(process_data):
+    """
+
+    :param process_data:
+    """
+    # % of games a team chose batting first, won the match
+
+    winner_bat = pd.DataFrame(process_data[["Team1", "Team2", "toss_winner", "toss_decision", "match_winner"]])
+
+    winner_bat['Bat_Won'] = np.where((winner_bat['toss_winner'] == winner_bat['match_winner']) & (
+            winner_bat['toss_decision'] == "bat"), "Won the toss and chose to Bat First",
+                                     np.where((winner_bat['toss_winner'] == winner_bat['match_winner']),
+                                              "Won the toss and chose to Bat Second", " Lost the Toss "))
+    winner_bat.dropna(inplace=True)
+    count_df = pd.DataFrame(winner_bat.groupby("Bat_Won")["Bat_Won"].count())
+
+    count_df.rename(columns={"Bat_Won": "count"}, inplace=True)
+
+    count_df.reset_index(inplace=True)
+
+    fig = px.pie(count_df, values="count", names='Bat_Won')
+
+    fig.update_layout(title_text=f'Toss results vs % of Matches Won for :  {season} Season')
+
+    fig.show()
+
 
 # --------------------------------------------------------------------------------------------------------------------
 
+# # How does Toss Decision has any relation with that team winning the match or not
+#
+# match_won = pd.DataFrame(toss_df.groupby(["match_winner", "toss_decision"])["match_winner"].count())
+# match_won = match_won.rename(columns={'match_winner': 'Number of Matches Won'})
+# match_won.reset_index(inplace=True)
+# match_won = match_won.rename(columns={'match_winner': "Match Winner"})
+# match_won.head()
+#
+# colors = plotly.colors.qualitative.Prism
+# fig = px.bar(match_won, x='Match Winner', y='Number of Matches Won', color='toss_decision',
+#              text=match_won['Number of Matches Won'].astype(str), color_discrete_sequence=colors)
+# fig.show()
+#
+# # --------------------------------------------------------------------------------------------------------------------
+#
+# # % of games a team chose batting first, won the match
+#
+# winner_bat = pd.DataFrame(toss_df[["Team1", "Team2", "toss_winner", "toss_decision", "match_winner"]])
+#
+# winner_bat['Bat_Won'] = np.where((winner_bat['toss_winner'] == winner_bat['match_winner']) & (
+#         winner_bat['toss_decision'] == "bat"), "Bat First",
+#                                  np.where((winner_bat['toss_winner'] == winner_bat['match_winner']),
+#                                           "Bat Second", np.nan))
+#
+# ax = sns.countplot(x="Bat_Won", data=winner_bat)
+#
+# # --------------------------------------------------------------------------------------------------------------------
+#
+# # % of games a team won the toss, won the match
+#
+# winner_toss = pd.DataFrame(toss_df[["toss_winner", "match_winner"]])
+# winner_toss
+#
+# winner_toss['Toss_Won'] = np.where((winner_toss['toss_winner'] == winner_toss['match_winner']), "WonT_WonM",
+#                                    np.where((winner_toss['toss_winner'] != winner_toss['match_winner']), "WonT_LostM",
+#                                             np.nan))
+# winner_toss
+#
+# ax = sns.countplot(x="Toss_Won", data=winner_toss)
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    season = 2018
+
+    toss_df = pd.read_csv("ipl_match_info.csv",
+                          usecols=['Team1', 'Team2', 'date', 'venue', 'toss_winner', 'toss_decision',
+                                   'match_winner', 'year'])
+    process_df = process(toss_df, season)
+
+    overall_plot(process_df)
+
+    toss_plot(process_df)
+
+    decision_plot(process_df)
